e596a7d1a4e3d54fd580f4e1d87b536f251aaadf
JoeHitHard <joseph.meghanath@gmail.com>
2024-01-31 09:54:47 -0600

    basic request and response read

---
+++ UI/ride-share-ui/src/Pages/HomePage.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const HomePageText = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/rs/home/text');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data && (
+        <div>
+          <pre>{data}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HomePageText;
