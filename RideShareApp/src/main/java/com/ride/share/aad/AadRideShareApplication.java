4008a471b11695f47a8c5ed442df866ade3bea7a
JoeHitHard <joseph.meghanath@gmail.com>
2024-01-31 01:15:20 -0600

    backend initial commit

---
+++ RideShareApp/src/main/java/com/ride/share/aad/AadRideShareApplication.java
@@ -0,0 +1,13 @@
+package com.ride.share.aad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadRideShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AadRideShareApplication.class, args);
+	}
+
+}
---
+++ RideShareApp/src/test/java/com/ride/share/aad/AadRideShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ride.share.aad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AadRideShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
