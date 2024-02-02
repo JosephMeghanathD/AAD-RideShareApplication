af4b87ab0e56fd5311e0182ed4bff7adbfeb1fa4
JoeHitHard <joseph.meghanath@gmail.com>
2024-02-02 03:34:43 -0600

    users test

---
+++ RideShareApp/src/test/java/com/ride/share/aad/storage/entity/UserTest.java
@@ -0,0 +1,24 @@
+package com.ride.share.aad.storage.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+}
